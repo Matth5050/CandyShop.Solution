afb3463fb81dd0d565c1eebad1c103fc21b9e365
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-12 16:10:28 -0700

    updated edit method

--- CandyShop/Controllers/FlavorController.cs
+++ CandyShop/Controllers/FlavorController.cs
@@ -60,12 +60,17 @@ namespace CandyShop.Controllers
     public ActionResult Edit(int id)
     {
       var thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
       return View(thisFlavor);
     }
 
     [HttpPost]
-    public ActionResult Edit(Flavor flavor)
+    public ActionResult Edit(Flavor flavor, int TreatId)
     {
+      if (TreatId != 0)
+      {
+        _db.TreatFlavors.Add(new TreatFlavor() { TreatId = TreatId, FlavorId = flavor.FlavorId });
+      }
       _db.Entry(flavor).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
