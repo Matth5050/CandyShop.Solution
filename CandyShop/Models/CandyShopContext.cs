2a566b292dc415b076a3d215763c39b1900f1b37
Matth5050 <matthew.f.herbert@gmail.com>
2022-08-12 09:22:33 -0700

    Added controllers and models directories

---
+++ CandyShop/Models/CandyShopContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace CandyShop.Models
+{
+  public class CandyShopContext : DbContext
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavors { get; set; }
+    
+    public CandyShopContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
